2160c157ec52428e594f7553bb3fce10b9448177
Antonio Pierro <antonio.pierro@gmail.com>
2012-09-28 11:49:10 +0200

    added the load fixture button/action

--- src/js/collections/tasks.js
+++ src/js/collections/tasks.js
@@ -15,25 +15,20 @@ define([
         storageName: 'CoCoTask',
 
         initialize: function () {
-            // TODO probably this code should be improved
-            _.bindAll(this, 'loadFixture');
+            _.bindAll(this, 'createModel');
             this.localStorage = new Backbone.LocalStorage(this.storageName);
             if (!localStorage[this.storageName]) {
-                _.each(fixture, this.loadFixture);
+                this.loadFixture();
             }
-
         },
 
-        // TODO this method should be moved in utils/fixtures
-        loadFixture: function (taskObject) {
-            this.create(taskObject);
+        loadFixture: function () {
+            _.each(fixture, this.createModel);
         },
 
-        // TODO this function is unused
-        todo: function () {
-            return this.filter(function (todo) {
-                return todo.get('type') === 'todo';
-            });
+        createModel: function (object) {
+            this.create(object);
         }
+
     });
 });
